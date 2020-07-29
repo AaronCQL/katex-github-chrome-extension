caf47e0e038df284fd0aad5e4feecca16064d1b9
Aaron Choo <choozy.aaron@gmail.com>
2020-07-29 09:24:25 +0800

    feat: replace all instances of \ with \\

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import renderMathInElement from "katex/dist/contrib/auto-render";
 
 const readme = document.getElementById("readme");
 
+// replaces all instances of single backslash \ to double backslash \\
+const treeWalker = document.createTreeWalker(readme, NodeFilter.SHOW_TEXT);
+while (treeWalker.nextNode()) {
+  const node = treeWalker.currentNode;
+  node.nodeValue = node.nodeValue.replace(/\\[\n\s\r]+/g, "\\\\");
+}
+
 renderMathInElement(readme, {
   delimiters: [
     { left: "$$", right: "$$", display: true },
@@ -10,4 +17,5 @@ renderMathInElement(readme, {
     { left: "\\[", right: "\\]", display: true },
   ],
   throwOnError: false,
+  strict: false,
 });
