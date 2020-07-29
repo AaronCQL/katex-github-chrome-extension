95a949663d18147a659b3c736f0e7cc0056c627d
Aaron Choo <choozy.aaron@gmail.com>
2020-07-29 09:25:29 +0800

    feat: properly configure rollup config with del/copy

--- rollup.config.js
+++ rollup.config.js
@@ -5,34 +5,48 @@ import {
   chromeExtension,
   simpleReloader,
 } from "rollup-plugin-chrome-extension";
+import del from "rollup-plugin-delete";
 
 import transformCss from "./utils/transformCss";
 
-export default {
-  input: "src/manifest.json",
-  output: {
-    dir: "dist",
-    format: "esm",
+export default [
+  {
+    input: "src/index.js",
+    output: {
+      dir: "dist",
+      format: "esm",
+    },
+    plugins: [
+      // delete the /dist folder
+      del({ targets: "dist/*", runOnce: true }),
+      // copy essential KaTeX files from /node_modules to /src
+      copy({
+        targets: [
+          // copy KaTeX required fonts to /src, so that it gets bundled into /dist
+          { src: "node_modules/katex/dist/fonts/*", dest: "src/fonts" },
+          // copy KaTeX required css to /src, so that it gets bundled into /dist
+          {
+            src: "node_modules/katex/dist/katex.css",
+            dest: "src",
+            transform: transformCss,
+          },
+        ],
+        copyOnce: true,
+      }),
+    ],
   },
-  plugins: [
-    // always put chromeExtension() before other plugins
-    chromeExtension(),
-    simpleReloader(),
-    resolve(),
-    commonjs(),
-    copy({
-      targets: [
-        // copy KaTeX required fonts to /src, so that it gets bundled into /dist
-        { src: "node_modules/katex/dist/fonts/*", dest: "src/fonts" },
-        // copy KaTeX required css to /src, so that it gets bundled into /dist
-        {
-          src: "node_modules/katex/dist/katex.css",
-          dest: "src",
-          transform: transformCss,
-        },
-      ],
-      hook: "buildStart",
-      copyOnce: true,
-    }),
-  ],
-};
+  {
+    input: "src/manifest.json",
+    output: {
+      dir: "dist",
+      format: "esm",
+    },
+    plugins: [
+      // always put chromeExtension() before other plugins
+      chromeExtension(),
+      simpleReloader(),
+      resolve(),
+      commonjs(),
+    ],
+  },
+];
